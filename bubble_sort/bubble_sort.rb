14dc699449731fce1b466e953776647160174c7f
Philipp Bischoff <bischoff.phil@gmail.com>
2019-12-15 09:55:12 +0100

    add method bubble_sort_by

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -14,7 +14,28 @@ def bubble_sort(array)
     end
     i += 1
   end
-  p array
+  array
 end
 
-bubble_sort([4, 3, 78, 2, 0, 2])
+def bubble_sort_by(array)
+  swaps = 1
+  i = 1
+  while swaps.positive?
+    swaps = 0
+    (0...array.length - i).each do |index|
+      if yield(array[index], array[index + 1]).positive?
+        array[index], array[index + 1] = array[index + 1], array[index]
+        swaps += 1
+      end
+    end
+    i += 1
+  end
+  array
+end
+
+p bubble_sort([4, 3, 78, 2, 0, 2])
+
+sorted_array = bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
+  left.length - right.length
+end
+p sorted_array
